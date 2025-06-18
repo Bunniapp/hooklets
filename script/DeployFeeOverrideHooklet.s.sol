6e22f8c7f48643a394934cd4d255a64808634ce9
Szeth Vallano <0xSzeth@gmail.com>
2025-06-18 11:58:26 -0600

    chore: Hooklet flags check

--- script/DeployFeeOverrideHooklet.s.sol
+++ script/DeployFeeOverrideHooklet.s.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.13;
 
 import {LibString} from "solady/utils/LibString.sol";
 import {SafeCastLib} from "solady/utils/SafeCastLib.sol";
+import {HookletLib} from "bunni-v2/src/lib/HookletLib.sol";
 
 import {CREATE3Script} from "./base/CREATE3Script.sol";
 import {FeeOverrideHooklet} from "../src/FeeOverrideHooklet.sol";
@@ -15,9 +16,18 @@ contract DeployFeeOverrideHookletScript is CREATE3Script {
 
     function run() external returns (FeeOverrideHooklet feeOverrideHooklet, bytes32 feeOverrideHookletSalt) {
         uint256 deployerPrivateKey = uint256(vm.envBytes32("PRIVATE_KEY"));
+        address deployer = vm.addr(deployerPrivateKey);
+
         address bunniHub = vm.envAddress(string.concat("BUNNI_HUB_", block.chainid.toString()));
 
         feeOverrideHookletSalt = getCreate3SaltFromEnv("FeeOverrideHooklet");
+
+        uint256 feeOverrideHookletFlags = HookletLib.BEFORE_SWAP_FLAG + HookletLib.BEFORE_SWAP_OVERRIDE_FEE_FLAG;
+        address feeOverrideHookletDeployed = create3.getDeployed(deployer, feeOverrideHookletSalt);
+        require(
+            uint160(bytes20(feeOverrideHookletDeployed)) & HookletLib.ALL_FLAGS_MASK == feeOverrideHookletFlags && feeOverrideHookletDeployed.code.length == 0,
+            "hooklet address invalid"
+        );
 
         vm.startBroadcast(deployerPrivateKey);
 
