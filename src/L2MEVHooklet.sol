e9d47941b9f338ae066eccad1e0ffd7266fdac5c
ZeframLou <zeframlou@gmail.com>
2025-08-19 16:28:22 -0700

    feat: safer asset conversion using TWAP oracle

--- src/L2MEVHooklet.sol
+++ src/L2MEVHooklet.sol
@@ -23,6 +23,7 @@ import {IPriceOracle} from "euler-price-oracle/interfaces/IPriceOracle.sol";
 /// Implements MEV tax on swaps using priority fees, which only supports pairs with ETH/WETH as one asset.
 /// Also implements spot price override using Euler IPriceOracle contracts.
 contract L2MEVHooklet is IHooklet {
+    using TickMath for *;
     using FixedPointMathLib for *;
     using PoolIdLibrary for PoolKey;
     using CurrencyLibrary for Currency;
@@ -36,10 +37,12 @@ contract L2MEVHooklet is IHooklet {
         IPriceOracle oracle;
     }
 
+    uint24 internal constant TWAP_DURATION = 5 minutes;
     uint256 internal constant PRIO_FEE_MULT_BASE = 1000;
     uint256 internal constant Q64 = 1 << 64;
     uint256 internal constant Q160 = 1 << 160; // Q96 * Q64
     uint256 internal constant Q192 = 1 << 192; // Q96 * Q96
+    uint256 internal constant ORACLE_MIN_ETH_IN = 0.1 ether;
 
     IBunniHub public immutable bunniHub;
     Currency public immutable weth;
@@ -89,9 +92,9 @@ contract L2MEVHooklet is IHooklet {
             revert L2MEVHooklet__NotBunniTokenOwner();
         }
 
-        // one of the tokens must be ETH or WETH if priority fee is set
+        // one of the tokens must be ETH or WETH if priority fee and/or oracle is set
         if (
-            newConfig.priorityFeeMultiplier != 0
+            (newConfig.priorityFeeMultiplier != 0 || address(newConfig.oracle) != address(0))
                 && !(key.currency0 == CurrencyLibrary.ADDRESS_ZERO || key.currency0 == weth || key.currency1 == weth)
         ) {
             revert L2MEVHooklet__InvalidTokenPair();
@@ -148,13 +151,25 @@ contract L2MEVHooklet is IHooklet {
             // override price using oracle
             // always use the specified amount as inAmount, since if it's an exact output swap then we compute the price via a
             // hypothetical outputToken => inputToken swap
+            bool currency0IsEth = key.currency0.isAddressZero() || key.currency0 == weth;
+            bool currency0IsSpecified = exactIn == params.zeroForOne;
+
             uint256 inAmount = params.amountSpecified.abs(); // in base token
-            bool token0IsSpecified = exactIn == params.zeroForOne;
-            (address base, address quote) = token0IsSpecified
+
+            // enforce minimum oracle input amount to ensure the price has enough precision
+            if (currency0IsEth == currency0IsSpecified) {
+                // ETH is the specified currency
+                inAmount = FixedPointMathLib.max(inAmount, ORACLE_MIN_ETH_IN);
+            } else {
+                // ETH is the unspecified currency
+                inAmount = FixedPointMathLib.max(inAmount, _convert(key, currency0IsEth, ORACLE_MIN_ETH_IN));
+            }
+
+            (address base, address quote) = currency0IsSpecified
                 ? (Currency.unwrap(key.currency0), Currency.unwrap(key.currency1))
                 : (Currency.unwrap(key.currency1), Currency.unwrap(key.currency0));
             uint256 outAmount = config.oracle.getQuote(inAmount, base, quote); // in quote token
-            (uint256 amount0, uint256 amount1) = token0IsSpecified ? (inAmount, outAmount) : (outAmount, inAmount);
+            (uint256 amount0, uint256 amount1) = currency0IsSpecified ? (inAmount, outAmount) : (outAmount, inAmount);
             if (amount0 == 0) {
                 // divide by zero error
                 // don't override price
@@ -204,31 +219,19 @@ contract L2MEVHooklet is IHooklet {
                 swapAmountInWei = params.amountSpecified.abs();
             } else {
                 // the unspecified amount is in ETH
-                // use sqrtPriceX96 to compute the corresponding ETH value
-                // and then compute the fee rate
-                (uint160 sqrtPriceX96_,,,) = IBunniHook(address(key.hooks)).slot0s(poolId);
-                uint256 amountSpecified = params.amountSpecified.abs();
-                if (currency0IsEth) {
-                    uint256 invSqrtPriceX64 = Q160 / sqrtPriceX96_; // unit: sqrt(token0 / token1) in Q64
-                    // squaring invSqrtPriceX64 is safe because log2((Q160 / MIN_SQRT_PRICE)^2) = 255.9998915435 < 256
-                    uint256 priceOf1In0X64 = invSqrtPriceX64.mulDiv(invSqrtPriceX64, Q64); // unit: token0 / token1 in Q64
-                    swapAmountInWei = amountSpecified.fullMulDiv(priceOf1In0X64, Q64);
-                } else {
-                    uint256 sqrtPriceX64 = sqrtPriceX96_ >> 32; // unit: sqrt(token1 / token0) in Q64
-                    // squaring sqrtPriceX64 is safe because log2((MAX_SQRT_PRICE >> 32)^2) = 255.9998915440 < 256
-                    uint256 priceOf0In1X64 = sqrtPriceX64.mulDiv(sqrtPriceX64, Q64); // unit: token0 / token1 in Q64
-                    swapAmountInWei = amountSpecified.fullMulDiv(priceOf0In1X64, Q64);
-                }
+                swapAmountInWei = _convert(key, !currency0IsEth, params.amountSpecified.abs());
             }
-            // uint24 cast is safe since fee is capped at SWAP_FEE_BASE - 1
-            fee += uint24(
-                FixedPointMathLib.min(
-                    priorityFeeInWei.mulDivUp(SWAP_FEE_BASE, swapAmountInWei).mulDivUp(
-                        config.priorityFeeMultiplier, PRIO_FEE_MULT_BASE
-                    ),
-                    SWAP_FEE_BASE - 1 - fee
-                )
-            );
+            if (swapAmountInWei != 0) {
+                // uint24 cast is safe since fee is capped at SWAP_FEE_BASE - 1
+                fee += uint24(
+                    FixedPointMathLib.min(
+                        priorityFeeInWei.mulDivUp(SWAP_FEE_BASE, swapAmountInWei).mulDivUp(
+                            config.priorityFeeMultiplier, PRIO_FEE_MULT_BASE
+                        ),
+                        SWAP_FEE_BASE - 1 - fee
+                    )
+                );
+            }
         }
     }
 
@@ -363,5 +366,40 @@ contract L2MEVHooklet is IHooklet {
         uint256 /* orderOutputAmount */
     ) external pure returns (bytes4 selector) {
         return IHooklet.afterRebalance.selector;
+    }
+
+    /// -----------------------------------------------------------------------
+    /// Internal functions
+    /// -----------------------------------------------------------------------
+
+    function _convert(PoolKey calldata key, bool zeroToOne, uint256 fromAmount)
+        internal
+        view
+        returns (uint256 toAmount)
+    {
+        // use TWAP sqrtPriceX96 to compute the corresponding ETH value
+        // and then compute the fee rate
+        uint160 sqrtPriceX96_ = _getTwap(key).getSqrtPriceAtTick();
+        if (!zeroToOne) {
+            uint256 invSqrtPriceX64 = Q160 / sqrtPriceX96_; // unit: sqrt(token0 / token1) in Q64
+            // squaring invSqrtPriceX64 is safe because log2((Q160 / MIN_SQRT_PRICE)^2) = 255.9998915435 < 256
+            uint256 priceOf1In0X64 = invSqrtPriceX64.mulDiv(invSqrtPriceX64, Q64); // unit: token0 / token1 in Q64
+            return fromAmount.fullMulDiv(priceOf1In0X64, Q64);
+        } else {
+            uint256 sqrtPriceX64 = sqrtPriceX96_ >> 32; // unit: sqrt(token1 / token0) in Q64
+            // squaring sqrtPriceX64 is safe because log2((MAX_SQRT_PRICE >> 32)^2) = 255.9998915440 < 256
+            uint256 priceOf0In1X64 = sqrtPriceX64.mulDiv(sqrtPriceX64, Q64); // unit: token1 / token0 in Q64
+            return fromAmount.fullMulDiv(priceOf0In1X64, Q64);
+        }
+    }
+
+    function _getTwap(PoolKey calldata poolKey) internal view returns (int24 arithmeticMeanTick) {
+        IBunniHook hook = IBunniHook(address(poolKey.hooks));
+        uint32[] memory secondsAgos = new uint32[](2);
+        secondsAgos[0] = TWAP_DURATION;
+        secondsAgos[1] = 0;
+        int56[] memory tickCumulatives = hook.observe(poolKey, secondsAgos);
+        int56 tickCumulativesDelta = tickCumulatives[1] - tickCumulatives[0];
+        return int24(tickCumulativesDelta / int56(uint56(TWAP_DURATION)));
     }
 }
